f547a1a2469fb6a7f53b784e39676bbcdeaecbcd
Tunde James <tundejames04@gmail.com>
2024-07-31 20:44:47 +0100

    Update the types for jobs in JobList component

--- app/jobs/job-list.tsx
+++ app/jobs/job-list.tsx
@@ -1,9 +1,31 @@
 import JobCard from "./job-card";
 import { promises as fs } from "fs";
 
+interface Job {
+  id: number;
+  company: string;
+  logo: string;
+  logoBackground: string;
+  position: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  website: string;
+  apply: string;
+  description: string;
+  requirements: {
+    content: string;
+    items: string[];
+  };
+  role: {
+    content: string;
+    items: string[];
+  };
+}
+
 async function JobList() {
   const data = await fs.readFile(process.cwd() + "/app/data.json", "utf8");
-  const jobs = JSON.parse(data);
+  const jobs: Job[] = JSON.parse(data);
 
   return (
     <div className="container grid grid-cols-1 tablet:w-[689px] tablet:grid-cols-2 tablet:gap-3 desktop:w-[1110px] desktop:grid-cols-3">
